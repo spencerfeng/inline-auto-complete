ff5bfd68799e578ac229e6cb599fec051a0d2fbc
Spencer <spencer@sana.com.au>
2020-05-11 19:59:25 +1000

    feat: update the content and styling of Suggestions component

--- src/components/Suggestions.tsx
+++ src/components/Suggestions.tsx
@@ -6,8 +6,33 @@ interface Props {
 
 type Ref = HTMLDivElement
 
+const styles = {
+  wrapper: {
+    width: 100,
+    border: '1px solid #cccccc'
+  },
+  list: {
+    listStyle: 'none',
+    margin: 0,
+    padding: 0,
+    textAlign: 'left'
+  } as React.CSSProperties,
+  listItem: {
+    padding: '5px 15px'
+  }
+}
+
 const Suggestions = React.forwardRef<Ref, Props>((props: Props, ref) => {
-  return <div ref={ref}>{props.text}</div>
+  return (
+    <div ref={ref} style={styles.wrapper}>
+      <ul style={styles.list}>
+        <li style={styles.listItem}>Item 1</li>
+        <li style={styles.listItem}>Item 2</li>
+        <li style={styles.listItem}>Item 3</li>
+        <li style={styles.listItem}>Item 4</li>
+      </ul>
+    </div>
+  )
 })
 
 Suggestions.displayName = 'Suggestions'
